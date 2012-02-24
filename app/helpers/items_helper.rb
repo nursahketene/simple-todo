435424d891bc568af205ac56f16c6db4e9572581
Nur Sah Ketene <nursahketene@gmail.com>
2012-02-24 02:17:47 +0200

    Showing time of update in words according to their distance to Time.now

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,13 @@
 module ItemsHelper
+	
+	def date_status(item)
+		if (Time.now.in_time_zone - item.updated_at.to_datetime)  <= 24.hours 
+			time_ago_in_words(item.updated_at)
+		elsif(Time.now.in_time_zone - item.updated_at.to_datetime) <= 1.year
+			item.updated_at.strftime("%d %b")
+		else
+			item.update_at.strftime("%d %b %Y")
+		end
+	end
+
 end
