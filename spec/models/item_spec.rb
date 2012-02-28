a9ee51ff7b2af4eb554cfa9fc27f127b72ed6643
Nur Sah Ketene <nursahketene@gmail.com>
2012-02-28 17:54:41 +0200

    Test for items should not be empty.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,9 @@ describe Item do
   it "should create an item" do
     Factory(:item).should_not eq(nil)
   end
+  it "should retrieve todo items" do
+    Factory(:item)
+    
+    Item.todo.should_not be_empty
+  end
 end
