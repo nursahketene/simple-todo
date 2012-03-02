4fbf80ff3ae42d561f23a767b9b3ee3692d6772b
Nur Sah Ketene <nursahketene@gmail.com>
2012-03-02 11:45:49 +0200

    Fixed the order of the items to DESC

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
   
-  scope :default_order, order(:updated_at)
+  scope :default_order, order("updated_at DESC")
   scope :todo, default_order.where(:done => false)
   scope :done, default_order.where(:done => true)
 
